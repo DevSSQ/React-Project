30944d444e93144acb1f4eae60618e782090ee76
Sarah H <sara.s.qahtanii@gmail.com>
2022-10-31 02:58:04 +0300

    adding login form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Page from './components/Page';
+import LoginForm from './components/Login';
   
 function App() {
   return (
     <div>
-      <Nav/>
+      {/* <Nav/>
       <div className="App">
       <Page name = {'JavaScript'} description = {'Java allows you to build and make web pages interactive.'} expiry = {'20'} photo = {'https://www.citypng.com/public/uploads/preview/js-javascript-round-logo-icon-png-11662226392lsrrajcm0y.png'} />
       <Page name = {'Python'} description = {'Programming language used to build websites and software.'} expiry = {'10'} photo = {'https://p.kindpng.com/picc/s/159-1595848_python-logo-png-transparent-background-python-logo-png.png'} />
@@ -15,9 +16,9 @@ function App() {
       <Page name = {'CSS'} description = {'Language we use to style an HTML document.'} expiry = {'22'} photo = {'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png'} />
       <Page name = {'Java'} description = {'High-level, object-oriented programming language.'} expiry = {'05'} photo = {'https://www.developer.com/wp-content/uploads/2021/09/Java-tutorials.jpg'} />
       </div>
-      <Footer/>
+      <Footer/> */}
+      <LoginForm/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React, {useState}from 'react';
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button
+} from '@chakra-ui/react';
+
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(`Email: ${email} & Password: ${password}`);
+  };
+  return (
+    <Flex width="full" align="center" justifyContent="center">
+      <Box 
+      p={8}
+      maxWidth="500px"
+      borderWidth={1}
+      borderRadius={8}
+      boxShadow="lg"
+      >
+        <Box textAlign="center">
+          <Heading>Login</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form onSubmit={handleSubmit}>
+            <FormControl isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input 
+              type="email" 
+              placeholder="test@test.com"
+              size="lg"
+              onChange={event => setEmail(event.currentTarget.value)}
+              />
+            </FormControl>
+            <FormControl isRequired mt={6} >
+              <FormLabel>Password</FormLabel>
+              <Input 
+              type="password" 
+              placeholder="******" 
+              size="lg"
+              onChange={event => setPassword(event.currentTarget.value)}
+              />
+            </FormControl>
+            <Button 
+            variantColor="teal"
+            variant="outline"
+            type="submit"
+            width="full"
+            mt={4}
+            >
+              Sign In
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
+}
